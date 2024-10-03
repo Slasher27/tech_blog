026cd04a960c30c8f28e509018b5f467ca2ad62b
Duwayne <dcowney@outlook.com>
2024-10-03 07:48:37 +0200

    new co

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Techpeople Blog';
 export const SITE_DESCRIPTION = 'Atricles, stories and guides about tech';
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
